4446b0a40d80576ddd836d7c81b512fb64af5768
Arif Jehda-Oh <25460850+arifjehoh@users.noreply.github.com>
2022-11-06 00:11:01 +0100

    #2 logging n actuator (#3)
    * Added directory to use mvn exe in directory
    
    * Added logging
    
    * Added actuator

--- template/dockerfile/function/src/main/java/arifjehoh/open/application/Application.java
+++ template/dockerfile/function/src/main/java/arifjehoh/open/application/Application.java
@@ -1,5 +1,7 @@
 package arifjehoh.open.application;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -9,13 +11,18 @@ import java.util.function.Function;
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    private static final Logger logger = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
 
-	@Bean
-	public Function<String, String> function() {
-		return value -> "TODO Create your function here";
-	}
+    @Bean
+    public Function<String, String> function() {
+        return value -> {
+            logger.info("Function have been made. Value: {}", value);
+            return "TODO Create your function here";
+        };
+    }
 }
